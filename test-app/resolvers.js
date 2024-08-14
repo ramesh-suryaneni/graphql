7a5731b3a8e78f9028e385d6331bec8324616acf
ramesh-suryaneni <ramesh.suryaneni@gmail.com>
2024-08-14 19:54:33 +0530

    nested query

--- test-app/resolvers.js
+++ test-app/resolvers.js
@@ -2,5 +2,15 @@ const db = require('./db');
 const Query = {
     test: () => { return "Test Success, GraphQL server is up & running !!";},
     students:() => {return db.students.list();}
+ },
+ 
+ //for each single student object returned,resolver is invoked
+const Student = {
+    fullName:(root,args,context,info) => {
+       return root.firstName+":"+root.lastName
+    },
+    college:(root) => {
+        return db.colleges.get(root.collegeId);
+     }
  }
- module.exports = {Query}
+ module.exports = {Query, Student}
