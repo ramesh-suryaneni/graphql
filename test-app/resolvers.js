370f18377be15dedad26fe757cad9b3fa24232dd
ramesh-suryaneni <ramesh.suryaneni@gmail.com>
2024-08-14 20:23:55 +0530

    nested schema

--- test-app/resolvers.js
+++ test-app/resolvers.js
@@ -1,13 +1,19 @@
 const db = require('./db');
 const Query = {
     test: () => { return "Test Success, GraphQL server is up & running !!";},
-    students:() => {return db.students.list();}
- },
+    students:() => db.students.list(),
  
- //for each single student object returned,resolver is invoked
+    //resolver function for studentbyId
+    studentById:(root,args,context,info) => {
+    //args will contain parameter passed in query
+        return db.students.get(args.id);
+ }
+}
+
+//for each single student object returned,resolver is invoked
 const Student = {
     fullName:(root,args,context,info) => {
-       return root.firstName+":"+root.lastName
+       return root.firstName+"."+root.lastName
     },
     college:(root) => {
         return db.colleges.get(root.collegeId);
