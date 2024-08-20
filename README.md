5d1cefa95c7870d3cedea33522bf092bdd20e104
ramesh-suryaneni <ramesh.suryaneni@gmail.com>
2024-08-20 17:43:35 +0530

    mutations

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# graphql
+# overview
+this repo contains 2 samples;
+## 1. test-app
+a simple node-express apollo server implementation demonstrating various GraphQL basic concepts like schema, type, query, mutation and how to implement resolver functions for list of objects, custom and nested fields and using arguments etc.
+### how to run?
+`cd test-app`
+
+`npm install`
+
+`npm start`
+### test?
+click on browser http://localhost:9000/graphiql
+
+Test these queries & mutations on graphql browser using [tests.txt](/tests.txt)
+## 2. graphql-api
+This project aims to build a simplified e-commerce platform using GraphQL as the API layer and multiple microservices for handling different business domains. We'll focus on three core microservices: Product, Inventory, and Order.
+
+The GraphQL API will act as a unified interface for clients (web, mobile, etc.). It will fetch data from the microservices and resolve queries and mutations.
+### Microservices
+1. Product Microservice
+
+Manages product information (name, description, price, images, etc.)
+
+Handles product search and filtering
+
+Provides product recommendations
+
+2. Inventory Microservice
+
+Manages product stock levels
+
+Handles order fulfillment checks
+
+3. Order Microservice
+
+Manages customer orders (items, quantities, shipping address, payment details)
+
+Handles order status updates
+
+
  
--- test-app/resolvers.js
+++ test-app/resolvers.js
@@ -1,6 +1,13 @@
 const db = require('./db');
 const Query = {
-    test: () => { return "Test Success, GraphQL server is up & running !!";},
+    test: () => { 
+        return "Test Success, GraphQL server is up & running !!";
+    },
+    //using variable
+    sayHello:(root,args,context,info) => { 
+        return `Hi ${args.name} GraphQL server says Hello to you!!`;
+    },
+    
     students:() => db.students.list(),
  
     //resolver function for studentbyId
@@ -19,4 +26,28 @@ const Student = {
         return db.colleges.get(root.collegeId);
      }
  }
- module.exports = {Query, Student}
+
+ //Note that the function createStudent returns a String type. This is a unique identifier (ID) which is generated after creating a student.
+ const Mutation = {
+    createStudent:(root,args,context,info) => {
+       return db.students.create({
+        collegeId:args.collegeId,
+        firstName:args.firstName,
+        lastName:args.lastName,
+        email:args.email,
+        password:args.password})
+    },
+    // new resolver function
+    addStudent_returns_object:(root,args,context,info) => {
+        const id = db.students.create({
+        collegeId:args.collegeId,
+        firstName:args.firstName,
+        lastName:args.lastName,
+        email:args.email,
+        password:args.password})
+
+    return db.students.get(id)
+
+    }
+ }
+ module.exports = {Query, Mutation, Student}
